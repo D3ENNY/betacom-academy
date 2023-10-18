0f4e84df9aa8d8e7aa71cac6ce1ee73612aa6e9e
D3ENNY <denysraimondi06@gmail.com>
2023-10-18 13:03:15 +0200

    add encrypt file

---
+++ startaccademynet4/StartLesson8/Crypt.cs
@@ -0,0 +1,54 @@
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace startaccademynet4;
+
+class Encrypt
+{
+    internal string EncryptString(string sValue){
+        StringBuilder strB = new();
+        try
+        {
+                SHA256 sha256 = SHA256.Create();
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(sValue)); 
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    strB.Append(bytes[i].ToString("x2")); //c2 serve per convertire in HEX
+                } 
+        }
+        catch (System.Exception)
+        {
+            
+            throw;
+        }
+        return strB.ToString();
+    }
+
+    internal string EncryptSaltString(string sValue){
+        byte[] byteSalt = new byte[16];
+        string EncResult = string.Empty;
+        string EncSalt = string.Empty;
+        try
+        {
+            RandomNumberGenerator.Fill(byteSalt);
+            EncResult = Convert.ToBase64String(
+                //dotnet add package Microsoft.AspNetCore.Cryptography.KeyDerivation --version 7.0.12
+                KeyDerivation.Pbkdf2(   
+                    password: sValue,
+                    salt: byteSalt,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 10000,
+                    numBytesRequested: 132
+                )
+            );
+            EncSalt = Convert.ToBase64String(byteSalt);
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+        return EncSalt;
+    }
+}
