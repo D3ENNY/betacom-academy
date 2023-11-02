d58ad8ce00f5f28753b207046d9b5cc6e26b41bb
D3ENNY <denysraimondi06@gmail.com>
2023-11-02 14:54:40 +0100

    implements database manager

--- gestioneDipendenti/engine/manager/constant/Constant.cs
+++ gestioneDipendenti/engine/manager/constant/Constant.cs
@@ -2,8 +2,12 @@ namespace gestioneDipendenti.engine;
 
 internal static class Constant
 {
-    internal static string employerPath = "assets/database/Employees.txt";
-    internal static string employerActivityPath = "assets/database/EmployeesActivities.txt";
-    internal static string employerJson = "assets/output/out.json";
+    internal static string employerPath = "../../../assets/database/Employees.txt";
+    internal static string employerActivityPath = "../../../assets/database/EmployeesActivities.txt";
+    internal static string employerJson = "../../../assets/output/out.json";
     internal static char sepFile = ';';
+
+    //database
+
+    internal static string connStr = @"Data Source=.\sqlexpress;Inital Catalog=DipendentiAzienda;Integrated Security=True;";
 }
