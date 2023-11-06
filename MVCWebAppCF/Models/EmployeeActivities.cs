dfbedcf5895ab049de797d63d3004a565ec504b4
D3ENNY <denysraimondi06@gmail.com>
2023-11-06 17:18:37 +0100

    code first migration

---
+++ MVCWebAppCF/Models/EmployeeActivities.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCWebApp.Models
+{
+    public class EmployeeActivities
+    {
+        [Key]
+        public int Id { get; private set; }
+        public DateTime Date { get; set; }
+        public string Activity { get; set; } = "";
+        public int AmountHour { get; set; }
+        public string EmployerID { get; set; } = "";
+
+    }
+}
