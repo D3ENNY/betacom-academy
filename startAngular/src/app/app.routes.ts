a7443140bf497c0c8dc7172c08d30c4c1a538cf9
D3ENNY <denysraimondi06@gmail.com>
2023-11-27 23:04:44 +0100

    random test

--- startAngular/src/app/app.routes.ts
+++ startAngular/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { EmployeesComponent } from './features/Employees/employees.component';
 import { ActivitiesComponent } from './features/Activities/Activities.component';
 import { RegisterComponent } from './features/register/register.component';
+import { TestComponent } from './models/test/test.component';
 
 
 export const routes: Routes = [
     {path:'employee', component: EmployeesComponent},
     {path: 'employee/activities', component: ActivitiesComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'test', component: TestComponent},
     {path:'', redirectTo: 'home', pathMatch: 'full'}
 ];
