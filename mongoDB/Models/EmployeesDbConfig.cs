67aeb898d2574a12ce0cb0b4794ca20dc19dc920
D3ENNY <denysraimondi06@gmail.com>
2023-12-04 10:15:04 +0100

    remove lastest project

--- mongoDB/Models/EmployeesDbConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace mongoDB.Models
-{
-    public class EmployeesDbConfig
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string EmployeesCollectionName { get; set; }
-    }
-}
